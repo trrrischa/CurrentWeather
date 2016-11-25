1ab993f1fa0475f324bb70b71986ec5b7257b617
Ekaterina <trrrischa@MacBook-Pro-Ekaterina.local>
2016-11-25 12:55:38 +0500

    Initial Commit

---
+++ CurrentWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrentWeather
+//
+//  Created by Ekaterina on 25.11.16.
+//  Copyright © 2016 Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CurrentWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CurrentWeather
+//
+//  Created by Ekaterina on 25.11.16.
+//  Copyright © 2016 Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
